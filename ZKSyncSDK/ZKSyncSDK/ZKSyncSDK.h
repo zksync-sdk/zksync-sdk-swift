5d3f4ea5502706ccda54f386a90c8c2cc3db8a0a
Maxim Makhun <mmakhun@vareger.com>
2021-06-19 16:55:31 +0300

    Add `zks_crypto.h` to umbrella header to be able to build `ZKSyncSDK.xcodeproj` independently.

--- ZKSyncSDK/ZKSyncSDK/ZKSyncSDK.h
+++ ZKSyncSDK/ZKSyncSDK/ZKSyncSDK.h
@@ -15,4 +15,4 @@ FOUNDATION_EXPORT const unsigned char ZKSyncSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ZKSyncSDK/PublicHeader.h>
 
-
+#import "zks_crypto.h"
