41c184cf7f2d70ef87fc3653df2220dbf33f7257
Maxim Makhun <mmakhun@vareger.com>
2021-07-01 17:17:51 -0400

    Swift Package Manager support. (#4)

---
+++ ZKSyncCrypto/ZKSyncCrypto/ZKSyncCrypto+Types.swift
@@ -0,0 +1,84 @@
+//
+//  ZKSyncCrypto+Types.swift
+//  ZKSyncCrypto
+//
+//  Made with ❤️ by Matter Labs on 10/23/20
+//
+
+import Foundation
+
+public class ZKPrimitive {
+    class var bytesLength: Int {
+        return 0
+    }
+    
+    private var content: Data
+    
+    public init(_ content: Data) {
+        assert(content.count == Self.bytesLength, "Incorrect data length. Should be \(Self.bytesLength)")
+        
+        self.content = content
+    }
+    
+    public convenience init(_ content: [UInt8]) {
+        self.init(Data(content))
+    }
+    
+    public func data() -> Data {
+        return content
+    }
+    
+    public func base64String() -> String {
+        return content.base64EncodedString()
+    }
+    
+    public func hexEncodedString() -> String {
+        return content.map { String(format: "%02hhX", $0) }.joined()
+    }
+    
+    func withUnsafeBytes<ResultType>(_ body: (UnsafeRawBufferPointer) throws -> ResultType) rethrows -> ResultType {
+        return try! content.withUnsafeBytes(body)
+    }
+}
+
+public class ZKPackedPublicKey: ZKPrimitive {
+    override class var bytesLength: Int {
+        return 32
+    }
+}
+
+public class ZKPrivateKey: ZKPrimitive {
+    override public class var bytesLength: Int {
+        return 32
+    }
+}
+
+public class ZKPublicHash: ZKPrimitive {
+    override class var bytesLength: Int {
+        return 20
+    }
+}
+
+public class ZKSignature: ZKPrimitive {
+    override class var bytesLength: Int {
+        return 64
+    }
+}
+
+public class ZKResqueHash: ZKPrimitive {
+    override class var bytesLength: Int {
+        return 31
+    }
+}
+
+public enum ZKSyncCryptoError: Error {
+    case musigTooLongError
+    case seedTooShortError
+    case unsupportedOperation
+}
+
+public typealias CLibZksPrivateKey = ZksPrivateKey
+public typealias CLibZksPackedPublicKey = ZksPackedPublicKey
+public typealias CLibZksPubkeyHash = ZksPubkeyHash
+public typealias CLibZksSignature = ZksSignature
+public typealias CLibZksResqueHash = ZksResqueHash
